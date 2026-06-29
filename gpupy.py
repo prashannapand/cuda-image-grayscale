03cca72f00ee3067dadbc4d30b65403360c2bd7f
prashanna pandit <prashannapandit832@gmail.com>
2026-06-29 22:36:03 +0545

    made it a littel clear to see

--- gpupy.py
+++ gpupy.py
@@ -32,23 +32,31 @@ def run_graysclae():
              return 
          output_img=os.path.join(output_im,"output.bin")
          height,width,channel=img.shape
+
          img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
          input_file="img_rgb.bin"
          img_rgb.tofile(input_file)
          print(" file saved")
+
          executable="./gpu" if os.name!='nt' else"gpu.exe"
          cmd=[executable,str(height),str(width),input_file,output_img]
+
          print("launching gpu kernel")
          subprocess.run(cmd,capture_output=True,text=True)
          print(f"height is {height},weidth is {width},channel is{channel} ")
+
          gray_data=np.fromfile(output_img,dtype=np.uint8)
          gray_img=gray_data.reshape(height,width)
+
          output_gray_img=os.path.join(output_im,"output.png")
          cv2.imwrite(output_gray_img,gray_img)
+
          if os.path.exists(output_img):
              os.remove(output_img)
+             
          if os.path.exists(input_file):
              os.remove(input_file)
+             
       except Exception as e:
          print(f"error{e}")
          
