8ea5f704a3a89e92832f16ee9ed133d67a76ceda
prashanna pandit <prashannapandit832@gmail.com>
2026-07-10 14:01:19 +0545

    ade it interactable

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A high-performance, hybrid-architecture simulation of Conway's Game of Life. This engine offloads computationally heavy cellular automata updates to thousands of parallel GPU cores via a custom C++/CUDA DLL, utilizing Python and Pygame strictly as a lightweight presentation layer.
 
-## 🚀 Key Architectural Features
+##  Key Architectural Features
 
 * **Cross-Language Interoperability:** Implements a flat C-style interface (`extern "C"`) to seamlessly bridge high-level managed memory environments (Python/NumPy) with raw hardware pointers using the `ctypes` library.
 * **Parallel Core Mapping:** Maps 2D simulation coordinates directly onto hardware thread groups using CUDA's parallel execution configuration abstractions (`blockIdx`, `threadIdx`).
--- gameoflife_1.py
+++ gameoflife_1.py
@@ -19,7 +19,7 @@ cuda_engine.constructor.argtypes=[
     ctypes.c_int
 ]
 cuda_engine.constructor.restype=ctypes.c_int
-current_grid=np.random.choice([0,1],size=row*col,p=[0.8,0.2]).astype(np.int32)
+current_grid=np.random.choice([0,1],size=row*col,p=[1,0]).astype(np.int32)
 next_grid=np.zeros(row*col,dtype=np.int32)
 
 in_grid=current_grid.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
@@ -27,12 +27,25 @@ out_grid=next_grid.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
 
 a=cuda_engine.constructor(in_grid,out_grid,row,col)
 print(a)
-
+drawing=True
 running=True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
          running=False
+        if event.type==pygame.KEYDOWN:
+          if event.key==pygame.K_SPACE:
+            drawing=False
+        if drawing:
+            if drawing and event.type==pygame.MOUSEBUTTONDOWN:
+             if event.button==1:
+                  mouse_x,mouse_y=pygame.mouse.get_pos()
+                  click_x,click_y=mouse_x//cell,mouse_y//cell
+                  if(click_x<col and click_y<row):
+                        if(current_grid[click_x+click_y*col]==1):
+                           current_grid[click_x+click_y*col]=0
+                        else:
+                           current_grid[click_x+click_y*col]=1
     screen.fill(color=(10,10,10))
     grid_2d=current_grid.reshape(row,col)
     for r in range(row):
@@ -42,8 +55,9 @@ while running:
                 screen,(255,255,255),
                 (c * cell, r * cell, cell - 1, cell - 1)
              )
-    cuda_engine.gameoflife(in_grid,out_grid,row,col)
-    np.copyto(current_grid,next_grid)
+    if not drawing:
+       cuda_engine.gameoflife(in_grid,out_grid,row,col)
+       np.copyto(current_grid,next_grid)
     pygame.display.flip()
     clock.tick(5)
 pygame.quit()
